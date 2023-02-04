0a867dccb09c8611cc42b180c3d40b4027b15cb1
Cihat <44507004+yigitcihat@users.noreply.github.com>
2023-02-04 14:48:38 +0300

    Jump, Turning etc

--- Assets/[Game]/Scripts/SpiderBrain.cs
+++ Assets/[Game]/Scripts/SpiderBrain.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class SpiderBrain : MonoBehaviour
 {
     public Transform rayPoint;
+    private Transform _head;
     public float groundCheckDistance;
     public LayerMask groundLayer;
     public float yOffest;
@@ -17,21 +19,47 @@ public class SpiderBrain : MonoBehaviour
     public float breatheHeight;
     public float breatheSpeed;
     public LegMover[] legs;
+    Rigidbody2D RB;
+
+
     // Start is called before the first frame update
     void Start()
     {
         // PUT THIS ON THE BODY BONE
+        RB = GetComponent<Rigidbody2D>();
+        _head = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         currspeed = speed * Input.GetAxis("Horizontal");
+        if (currspeed < 0)
+        {
+            _head.localScale = new Vector3(1, -1, 1);
+        }
+        else
+        {
+            _head.localScale = Vector3.one;
+
+        }
         transform.position = new Vector3(transform.position.x + currspeed * Time.deltaTime, transform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
         CalculateGround();
         Idle();
     }
+    public void Jump()
+    {
+        float force = 5;
+        if (RB.velocity.y < 0)
+            force -= RB.velocity.y;
 
+        RB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
     public void CalculateGround()
     {
         float offset = 0;
