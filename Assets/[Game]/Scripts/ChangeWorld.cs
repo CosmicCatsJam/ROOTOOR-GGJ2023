1b77be02387669b6cf35bbc29ab750e6d6fdda3f
Ayşenur Yavuz <aysenur.fcb@gmail.com>
2023-02-05 02:44:05 +0300

    ++

--- Assets/[Game]/Scripts/ChangeWorld.cs
+++ Assets/[Game]/Scripts/ChangeWorld.cs
@@ -6,6 +6,7 @@ using Cinemachine;
 
 public class ChangeWorld : MonoBehaviour
 {
+    public CinemachineVirtualCamera NormalCam;
     public CinemachineVirtualCamera UpsideCam;
     CapsuleCollider[] colliders;
     Rigidbody2D rb;
@@ -31,23 +32,29 @@ public class ChangeWorld : MonoBehaviour
     {
         if (worldType == WorldType.Normal)
         {
+            CurrentPlayer.transform.root.gameObject.SetActive(false);
+
             worldType = WorldType.UpsideDown;
             var oppositeObj = Instantiate(UpsideDownPrefab);
             oppositeObj.transform.position = CurrentPlayer.transform.position + new Vector3(0, -14, 0);
             CurrentPlayer = oppositeObj.transform.GetChild(1).GetChild(0).gameObject;
             UpsideCam.Priority = 11;
-            gameObject.SetActive(false);
+            UpsideCam.m_Follow = CurrentPlayer.transform;
+            UpsideCam.m_LookAt = CurrentPlayer.transform;
         }
         else
         {
+            CurrentPlayer.transform.root.gameObject.SetActive(false);
+
             worldType = WorldType.Normal;
 
             UpsideCam.Priority = 9;
             var oppositeObj = Instantiate(NormalPrefab);
             oppositeObj.transform.position = CurrentPlayer.transform.position + new Vector3(0, 14, 0);
             CurrentPlayer = oppositeObj.transform.GetChild(1).GetChild(0).gameObject;
+            NormalCam.m_Follow = CurrentPlayer.transform;
+            NormalCam.m_LookAt = CurrentPlayer.transform;
 
-            gameObject.SetActive(false);
 
         }
 
