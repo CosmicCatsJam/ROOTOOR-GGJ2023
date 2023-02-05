f0704f9f7c1145bbf6a83e985af6d45c2c9928a5
Ayşenur Yavuz <aysenur.fcb@gmail.com>
2023-02-05 13:09:29 +0300

    +

--- Assets/[Game]/Scripts/Player/Player.cs
+++ Assets/[Game]/Scripts/Player/Player.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class Player : MonoBehaviour
 {
     public bool isUpsideDown;
+    public bool canRootable;
 
     public GameObject Sprites;
 
@@ -21,18 +22,37 @@ public class Player : MonoBehaviour
 
     void OpenAnim(int value)
     {
-        if (value ==0 )
+        if (value == 0)
         {
             var sprites = Instantiate(Sprites);
             sprites.transform.position = transform.position + new Vector3(0, -5, 0);
         }
-        else 
+        else
         {
             var sprites = Instantiate(Sprites);
-            sprites.transform.DORotate(new Vector3(0,0,180),0.1f);
+            sprites.transform.DORotate(new Vector3(0, 0, 180), 0.1f);
             sprites.transform.position = transform.position + new Vector3(0, 3, 0);
         }
-       
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6 && collision.gameObject.tag =="Rootable")
+        {
+            Debug.Log("rootable true");
+            canRootable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6 && collision.gameObject.tag == "Rootable")
+        {
+            Debug.Log("rootable false");
+            canRootable = false;
+
+        }
     }
 
 
