bceef5154d04ab0cca469041f29e7fd1f46c91a5
Cihat <44507004+yigitcihat@users.noreply.github.com>
2023-02-05 03:25:50 +0300

    UI

---
+++ Assets/[Game]/Scripts/UI/StartGamePanel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class StartGamePanel : MonoBehaviour
+{
+    public  TextMeshProUGUI GameName;
+    private Image gamePanelImage;
+
+
+    private void Start()
+    {
+        gamePanelImage = GetComponent<Image>();
+        gamePanelImage.material.DOFade(1, 0.1f);
+        GameName.transform.localScale = Vector3.one;
+    }
+    private void OnDisable()
+    {
+        gamePanelImage.material.DOFade(1, 0.1f);
+        GameName.transform.localScale= Vector3.one;
+    }
+    public void  StartGame()
+    {
+
+        StartCoroutine(ScaleAndTransparent());
+
+    }
+    IEnumerator ScaleAndTransparent()
+    {
+       
+        GameName.transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), 0.4f);
+        gamePanelImage.material.DOFade(0, 0.5f).OnComplete(()=> gameObject.SetActive(false));
+        yield return new WaitForSeconds(0.2f);
+    }
+}
