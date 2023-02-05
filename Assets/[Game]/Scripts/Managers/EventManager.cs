f23b83558777e87021a04d3adeb2df5a9da007fd
Cihat <44507004+yigitcihat@users.noreply.github.com>
2023-02-05 14:22:26 +0300

    Collecteables

--- Assets/[Game]/Scripts/Managers/EventManager.cs
+++ Assets/[Game]/Scripts/Managers/EventManager.cs
@@ -4,6 +4,7 @@ public static class EventManager
 {
     public static IntEvent OnUpsideDownWorldTransition = new IntEvent();
     public static UnityEvent OnLevelUp = new UnityEvent();
+    public static UnityEvent OnCollect = new UnityEvent();
 }
 
 public class IntEvent : UnityEvent<int> { }
